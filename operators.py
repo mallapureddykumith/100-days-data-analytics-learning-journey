cb4da0c0e4886297995219433193145e4238767a
mallapureddykumith <145972991+mallapureddykumith@users.noreply.github.com>
2026-07-22 21:20:48 +0530

    Day 1: Added Python practice files
    Uploaded my Day 1 Python practice files covering operators and basic concepts.

---
+++ operators.py
@@ -0,0 +1,121 @@
+'''
+tokens are smallest individual parts or units in the program
+
+1. keywords
+------------
+2. identifiers
+------------
+variable
+function name
+class name
+
+3. operators
+-------------
++ - =
+
+4. literal
+---------
+int, str, tuple, list
+
+operators
+--------
+1. arithamatic
+-------------
+-,+,/,%,*,**
+ex- num=8
+num_2=7
+print(num+num_2)
+print(num-num_2)
+print(num*num_2)
+print(num**num_2)
+print(num%num_2)
+print(num/num_2)
+
+2. assignment
+----------------
+=, +=, -=, *=, %=, /=
+ex- num=8
+num_2=7
+for j in range(10):
+num += 1
+print(num)
+
+3. comparision
+-----------------
+==, !=, <=, >=, >, <
+ex- num=8
+num_2=8
+print(num==num_2)
+print(num!=num_2)
+print(num<=num_2)
+print(num>=num_2)
+print(num>num_2)
+print(num<num_2)
+
+
+4. logical
+----------------
+and or not
+
+ex-for and
+
+num=78
+num_2=89
+print(num>num_2 and num_2<100)
+
+for- or
+
+num=78
+num_2=89
+print(num>num_2 and num_2<100)
+print(num>num_2 or num_2<100)
+print(not(num<num_2 and num_2<100))
+
+5. identity
+-----------------
+is
+is not
+ex- num=9
+num_2=9
+print(num is num_2)
+ex-2
+num=9
+num_2=9
+so=[1,2]
+how=[1,2]
+print(id(so))
+print(id(how))
+print(so == how)
+print(num is num_2)
+*what is difference btw is and ==?
+
+6. membership
+in
+not in
+ex-so=[1,2,3,4,5]
+how=[1,2]
+print(7 not in so)
+
+7. bitwise
+&,|, ^,<<, >>
+ex-print(5&3)
+print(5|3)
+print(5<<3)
+print(5>>3)
+
+num = 0
+if
+for
+else
+return
+while
+and
+in
+
+
+'''
+
+print(5&3)
+print(5|3)
+print(5<<3)
+print(5>>3)
