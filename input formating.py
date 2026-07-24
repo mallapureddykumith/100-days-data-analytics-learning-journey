9bf486988738a82eb062665f55987fe3ff6bfaa4
mallapureddykumith <145972991+mallapureddykumith@users.noreply.github.com>
2026-07-24 21:37:37 +0530

    Day 5 & 6 Python Input Formatting
    Day 5 of my Python learning journey.
    
    In this session, I practiced:
    • Taking user input using the input() function
    • Converting input into different data types (int, float, list, tuple)
    • Understanding how Python handles user input
    • Working with input formatting techniques
    • Converting 24-hour time to 12-hour format
    • Solving practice problems with user input and formatting
    
    This file contains hands-on examples and exercises to improve my understanding of Python input handling and formatting concepts.
    
    #Python #PythonProgramming #100DaysOfCode #CodingJourney #LearningPython #GitHub

---
+++ input formating.py
@@ -0,0 +1,53 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======= RESTART: C:/Users/joshh/OneDrive/Desktop/python notes/input formating.py =======
+enter a num:55
+64
+
+======= RESTART: C:/Users/joshh/OneDrive/Desktop/python notes/input formating.py =======
+enter a num:7
+16
+<class 'int'>
+
+======= RESTART: C:/Users/joshh/OneDrive/Desktop/python notes/input formating.py =======
+enter: 7
+<class 'str'>
+
+
+======= RESTART: C:/Users/joshh/OneDrive/Desktop/python notes/input formating.py =======
+enter: []
+<class 'str'>
+
+======= RESTART: C:/Users/joshh/OneDrive/Desktop/python notes/input formating.py =======
+enter nums: 55
+[55]
+
+======= RESTART: C:/Users/joshh/OneDrive/Desktop/python notes/input formating.py =======
+enter nums: 55
+['55']
+
+======= RESTART: C:/Users/joshh/OneDrive/Desktop/python notes/input formating.py =======
+enter nums: 55
+(55,)
+
+======= RESTART: C:/Users/joshh/OneDrive/Desktop/python notes/input formating.py =======
+enter: (1,2,3)
+<class 'tuple'>
+[1,2,3]
+[1, 2, 3]
+
+======= RESTART: C:/Users/joshh/OneDrive/Desktop/python notes/input formating.py =======
+enter 24hr clock6.56
+6.56
+
+======= RESTART: C:/Users/joshh/OneDrive/Desktop/python notes/input formating.py =======
+enter 24hr clock23:45
+Traceback (most recent call last):
+  File "C:/Users/joshh/OneDrive/Desktop/python notes/input formating.py", line 38, in <module>
+    hours_ = int(part_[0])-12
+NameError: name 'part_' is not defined. Did you mean: 'parts_'?
+
+======= RESTART: C:/Users/joshh/OneDrive/Desktop/python notes/input formating.py =======
+enter 24hr clock23:45
+11 : 45 pm
